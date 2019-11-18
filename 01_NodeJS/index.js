95f6f4d033a205ff7e78cc609f5ee9574f419f71
NeilSCGH <neil.segard@gmail.com>
2019-11-18 18:05:52 +0100

    modules

--- 01_NodeJS/index.js
+++ 01_NodeJS/index.js
@@ -1,25 +1,4 @@
-// Import a module
 const http = require('http')
-// Import Node url module
-const url = require('url')
-
-const qs = require('querystring')
-
-const serverHandle = function (req, res) {
-  const route = url.parse(req.url)
-  const path = route.pathname 
-  const params = qs.parse(route.query)
-
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-
-  if (path === '/hello' && 'name' in params) {
-    res.write('Hello ' + params['name'])
-  } else {
-    res.write('Hello anonymous')
-  }
-
-  res.end();
-}
-
-const server = http.createServer(serverHandle);
+const handles = require('./handles')
+const server = http.createServer(handles.serverHandle);
 server.listen(8080)
