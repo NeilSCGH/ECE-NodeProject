0bdfbfdfd150912e649c25c7b893630da868952e
NeilSCGH <neil.segard@gmail.com>
2019-11-18 18:40:12 +0100

    initial commit

--- 02_ExpressJS_TypeScript/index.js
+++ 02_ExpressJS_TypeScript/index.js
@@ -4,6 +4,6 @@ app = express()
 app.set('port', 1337)
 
 app.listen(
-  app.get('port'), 
-  () => console.log(`server listening on ${app.get('port')}`)
+  app.get('port'),
+  () => console.log(`Server listening on ${app.get('port')}`)
 )
