2dd20682765e6db24f0b7a1ac725518cb9faf71b
NeilSCGH <neil.segard@gmail.com>
2019-11-18 18:47:18 +0100

    new app.get

--- 02_ExpressJS_TypeScript/index.js
+++ 02_ExpressJS_TypeScript/index.js
@@ -4,6 +4,23 @@ app = express()
 app.set('port', 1337)
 
 app.listen(
-  app.get('port'),
-  () => console.log(`Server listening on ${app.get('port')}`)
+    app.get('port'),
+    () => console.log(`Server listening on ${app.get('port')}`)
 )
+
+app.get(
+    '/hello/:name',
+    (req, res) => res.send("Hello " + req.params.name)
+)
+
+app.post('/', (req, res) => {
+    // POST
+})
+
+app.put('/', function (req, res) {
+        // PUT
+    })
+
+app    .delete('/', (req, res) => {
+        // DELETE
+    })
