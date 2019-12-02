d1f2d07d178ea248a04de0ac89487dd429855e05
NeilSCGH <neil.segard@gmail.com>
2019-12-02 09:06:45 +0100

    debug

---
+++ 03_Storage/src/server.ts
@@ -0,0 +1,50 @@
+import express = require('express')
+import { MetricsHandler } from './metrics'
+import path = require('path')
+import bodyparser = require('body-parser');
+
+const app = express()
+const port: string = process.env.PORT || '8082'
+app.use(express.static(path.join(__dirname, '/../public')))
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded())
+
+app.set('views', __dirname + "/../views")
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req: any, res: any) => {
+  res.write('Hello world')
+  res.end()
+})
+
+app.get('/hello/:name', (req: any, res: any) => {
+  res.render('hello.ejs', { name: req.params.name })
+})
+
+app.get('/metrics.json', (req: any, res: any) => {
+  MetricsHandler.get((err: Error | null, result?: any) => {
+    if (err) {
+      throw err
+    }
+    res.json(result)
+  })
+})
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Server is running on http://localhost:${port}`)
+})
+
+
+const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+
+app.post('/metrics/:id', (req: any, res: any) => {
+  dbMet.save(req.params.id, req.body, (err: Error | null) => {
+    if (err) throw err
+    res.status(200).send()
+  })
+})
