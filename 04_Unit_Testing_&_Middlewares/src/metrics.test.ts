2701e83995de0304f70b71db6c8e7ea2648106a7
NeilSCGH <neil.segard@gmail.com>
2019-12-07 19:49:20 +0100

    all the works will be in the week 4

--- 04_Unit_Testing_&_Middlewares/src/metrics.test.ts
+++ 04_Unit_Testing_&_Middlewares/src/metrics.test.ts
@@ -4,16 +4,9 @@ import { Metric, MetricsHandler } from './metrics'
 import { LevelDB } from "./leveldb"
 
 const dbPath: string = 'db_test'
-var dbMet: MetricsHandler = new MetricsHandler(dbPath)
-
-/*
-const a: number = 0
+var dbMet: MetricsHandler// = new MetricsHandler(dbPath)
 
 describe('Metrics', function () {
-  it('should save and get', function () {
-    expect(a).to.equal(0)
-  })
-
   before(function () {
     LevelDB.clear(dbPath)
     dbMet = new MetricsHandler(dbPath)
@@ -22,15 +15,14 @@ describe('Metrics', function () {
   after(function () {
     dbMet.db.close()
   })
-})
-*/
 
-describe('#get', function () {
-  it('should get empty array on non existing group', function () {
-    dbMet.get( (err: Error | null, result?: Metric[]) => {
-      expect(err).to.be.null
-      expect(result).to.not.be.undefined
-      expect(result).to.not.be.empty
+  describe('#get', function () {
+    it('should get empty array on non existing group', function () {
+      dbMet.get((err: Error | null, result?: Metric[]) => {
+        expect(err).to.be.null
+        expect(result).to.not.be.undefined
+        expect(result).to.not.be.empty
+      })
     })
   })
 })
--- 04_Unit_Testing_&_Middlewares/src/server.ts
+++ 04_Unit_Testing_&_Middlewares/src/server.ts
@@ -3,10 +3,17 @@ import { MetricsHandler } from './metrics'
 import path = require('path')
 import bodyparser = require('body-parser');
 import morgan = require('morgan')
+import session = require('express-session')
+import levelSession = require('level-session-store')
+import { UserHandler, User } from './user'
 
 const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
 const port: string = process.env.PORT || '8082'
 const app = express()
+const LevelStore = levelSession(session)
+const dbUser: UserHandler = new UserHandler('./db/users')
+const authRouter = express.Router()
+const userRouter = express.Router()
 
 app.use(express.static(path.join(__dirname, '/../public')))
 app.use(bodyparser.urlencoded())
@@ -14,12 +21,92 @@ app.use(bodyparser.json())
 app.use(morgan('dev'))
 app.set('view engine', 'ejs');
 app.set('views', __dirname + "/../views")
+app.use(authRouter)
+app.use('/user', userRouter)
+
+app.use(session({
+  secret: 'my very secret phrase',
+  store: new LevelStore('./db/sessions'),
+  resave: true,
+  saveUninitialized: true
+}))
+
+authRouter.get('/login', (req: any, res: any) => {
+  res.render('login')
+})
+
+authRouter.get('/signup', (req: any, res: any) => {
+  res.render('signup')
+})
+
+authRouter.get('/logout', (req: any, res: any) => {
+  delete req.session.loggedIn
+  delete req.session.user
+  res.redirect('/login')
+})
+
+app.post('/login', (req: any, res: any, next: any) => {
+  dbUser.get(req.body.username, (err: Error | null, result?: User) => {
+    if (err) next(err)
+    if (result === undefined || !result.validatePassword(req.body.password)) {
+      res.redirect('/login')
+    } else {
+      req.session.loggedIn = true
+      req.session.user = result
+      res.redirect('/')
+    }
+  })
+})
+
+
+userRouter.post('/', (req: any, res: any, next: any) => {
+  dbUser.get(req.body.username, function (err: Error | null, result?: User) {
+    if (!err || result !== undefined) {
+      res.status(409).send("user already exists")
+    } else {
+      dbUser.save(req.body, function (err: Error | null) {
+
+        if (err) next(err)
+
+        else res.status(201).send("user persisted")
+      })
+    }
+  })
+})
+
+userRouter.get('/:username', (req: any, res: any, next: any) => {
+  dbUser.get(req.params.username, function (err: Error | null, result?: User) {
+    if (err || result === undefined) {
+      res.status(404).send("user not found")
+    }
+    else res.status(200).json(result)
+  })
+})
+
+const authCheck = function (req: any, res: any, next: any) {
+  if (req.session.loggedIn) {
+    next()
+  } else res.redirect('/login')
+}
+
+app.get('/', authCheck, (req: any, res: any) => {
+  res.render('index', { name: req.session.username })
+})
+
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`Server is running on http://localhost:${port}`)
+})
+
 
 app.get('/', (req: any, res: any) => {
   res.write('Hello world')
   res.end()
 })
-
+/*
 app.get('/hello/:name', (req: any, res: any) => {
   res.render('hello.ejs', { name: req.params.name })
 })
@@ -33,16 +120,10 @@ app.get('/metrics.json', (req: any, res: any) => {
   })
 })
 
-app.listen(port, (err: Error) => {
-  if (err) {
-    throw err
-  }
-  console.log(`Server is running on http://localhost:${port}`)
-})
-
 app.post('/metrics/:id', (req: any, res: any) => {
   dbMet.save(req.params.id, req.body, (err: Error | null) => {
     if (err) throw err
     res.status(200).send()
   })
 })
+*/
